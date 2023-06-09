d1cd33b65a0e3d91c85d355de295a468baeaa18c
Daniel Laschanzky <dlaschanzky2@huskers.unl.edu>
2023-06-08 19:36:19 -0500

    some buttons and a header

---
+++ script.js
@@ -0,0 +1,9 @@
+let myLibrary = []
+
+function Book(title, author, pageNum, readStatus){
+  this.title = title
+  this.author = author
+  this.pageNum = parseInt(pageNum)
+  this.readStatus = readStatus 
+
+}
